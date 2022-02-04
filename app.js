672141feb1caa555e5d079d93f127dc538071081
Mo2024 <mohdosama2025@gmail.com>
2022-02-04 13:48:03 +0300

    Session and flash

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+app.use(session({ secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false }));
+
+app.get('/viewcount', (req, res) => {
+
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`YOU HAVE VIEWED THIS PAGE ${req.session.count} TIMES`)
+
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
